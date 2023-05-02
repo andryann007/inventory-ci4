3141ab62f1890b5b371d68f6d40c1a82e2e20325
Andryan <andryancoolz@gmail.cpm>
2023-05-02 19:26:12 +0700

    Restructure Database

--- database/database_inventory.sql
+++ database/database_inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 14, 2023 at 03:08 PM
+-- Generation Time: May 02, 2023 at 02:21 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -44,7 +44,11 @@ CREATE TABLE `data_barang_keluar` (
 INSERT INTO `data_barang_keluar` (`id_keluar`, `id_barang`, `tgl_keluar`, `qty_keluar`, `harga_satuan_keluar`, `total_harga_keluar`, `keterangan`) VALUES
 (1, 20, '2023-04-06', 3, 12000, 36000, 'Penjualan'),
 (2, 19, '2023-03-15', 100, 15000, 1500000, 'Beli Beras'),
-(3, 11, '2023-04-15', 3, 100000, 300000, 'Kadaluarsa');
+(3, 11, '2023-04-15', 3, 100000, 300000, 'Kadaluarsa'),
+(4, 1, '2023-04-15', 15, 7000, 105000, 'Penjualan'),
+(5, 1, '2023-04-12', 15, 7000, 105000, '-'),
+(6, 2, '2023-04-01', 10, 5000, 50000, '-'),
+(7, 3, '2023-04-05', 10, 10000, 100000, '-');
 
 -- --------------------------------------------------------
 
@@ -60,7 +64,7 @@ CREATE TABLE `data_barang_masuk` (
   `qty_masuk` int(11) NOT NULL,
   `harga_satuan_masuk` double NOT NULL,
   `total_harga_masuk` double NOT NULL,
-  `keterangan` varchar(200) NOT NULL
+  `keterangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -96,7 +100,8 @@ CREATE TABLE `data_retur_barang` (
 
 INSERT INTO `data_retur_barang` (`id_retur`, `id_barang`, `id_supplier`, `tgl_retur`, `qty_retur`, `harga_satuan_retur`, `total_harga_retur`, `keterangan`) VALUES
 (1, 1, 1, '2023-04-06', 10, 6500, 65000, 'Expired'),
-(2, 2, 3, '2023-03-01', 5, 6500, 32500, 'Salah Beli');
+(2, 2, 3, '2023-03-01', 5, 6500, 32500, 'Salah Beli'),
+(3, 3, 3, '2023-04-12', 5, 10000, 50000, 'Expired');
 
 -- --------------------------------------------------------
 
@@ -111,7 +116,7 @@ CREATE TABLE `data_stock` (
   `qty_stock` int(11) NOT NULL,
   `harga_satuan` double NOT NULL,
   `total_harga` double NOT NULL,
-  `status` varchar(50) NOT NULL
+  `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -119,9 +124,9 @@ CREATE TABLE `data_stock` (
 --
 
 INSERT INTO `data_stock` (`id_barang`, `nama_barang`, `kategori`, `qty_stock`, `harga_satuan`, `total_harga`, `status`) VALUES
-(1, 'Royco Ayam & Sapi (12 Sachet)', 'Bumbu Dapur', 15, 6500, 97500, 'Tersedia'),
-(2, 'Masako Ayam & Sapi (12 sachet)', 'Bumbu Dapur', 45, 5500, 247500, 'Tersedia'),
-(3, 'Mamasuka Kentucky (210 gr)', 'Bumbu Dapur', 20, 5000, 100000, 'Tersedia'),
+(1, 'Royco Ayam & Sapi (12 Sachet)', 'Bumbu Dapur', 0, 6500, 97500, 'Habis'),
+(2, 'Masako Ayam & Sapi (12 sachet)', 'Bumbu Dapur', 0, 5500, 247500, 'Habis'),
+(3, 'Mamasuka Kentucky (210 gr)', 'Bumbu Dapur', 5, 5000, 25000, 'Tersedia'),
 (4, 'Mamasuka Tepung Goreng Tempe 100gr', 'Bumbu Dapur', 20, 2000, 40000, 'Tersedia'),
 (5, 'Aqua Air Mineral 1 Dus (600 ml)', 'Minuman', 10, 50000, 500000, 'Tersedia'),
 (6, 'Le Minerale 600 ml (1 Dus)', 'Minuman', 10, 50000, 500000, 'Tersedia'),
@@ -129,7 +134,7 @@ INSERT INTO `data_stock` (`id_barang`, `nama_barang`, `kategori`, `qty_stock`, `
 (8, 'Indomie Goreng 1 Dus (40 pcs)', 'Makanan Instan', 10, 125000, 1250000, 'Tersedia'),
 (9, 'Indomie Soto Ayam 1 Dus (40 pcs)', 'Makanan Instan', 10, 125000, 1250000, 'Tersedia'),
 (10, 'Indomie Kari Ayam 1 Dus (40 pcs)', 'Makanan Instan', 10, 125000, 1250000, 'Tersedia'),
-(11, 'Sedaap Mie Goreng 1 Dus (40 Pcs)', 'Sembako', 7, 122000, 854000, 'Tersedia'),
+(11, 'Sedaap Mie Goreng 1 Dus (40 Pcs)', 'Sembako', 15, 122000, 1830000, 'Tersedia'),
 (12, 'Sedaap Mie Soto 1 Dus (40 Pcs)', 'Sembako', 10, 122000, 1220000, 'Tersedia'),
 (13, 'Better Sandwich Biscuit (20 pcs)', 'Makanan Ringan', 15, 18000, 270000, 'Tersedia'),
 (14, 'Beng Beng Wafer 1 Box (20 pcs)', 'Makanan Ringan', 20, 19000, 380000, 'Tersedia'),
@@ -153,9 +158,9 @@ INSERT INTO `data_stock` (`id_barang`, `nama_barang`, `kategori`, `qty_stock`, `
 CREATE TABLE `data_supplier` (
   `id_supplier` int(11) NOT NULL,
   `nama_supplier` varchar(150) NOT NULL,
-  `alamat` varchar(200) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `telp` varchar(18) NOT NULL
+  `telp` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -176,11 +181,11 @@ INSERT INTO `data_supplier` (`id_supplier`, `nama_supplier`, `alamat`, `email`, 
 
 CREATE TABLE `data_user` (
   `id_user` int(11) NOT NULL,
-  `nama_lengkap` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(150) NOT NULL,
   `email` varchar(150) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(25) NOT NULL,
-  `telp` varchar(13) NOT NULL,
+  `telp` varchar(20) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `tipe_akun` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -190,7 +195,7 @@ CREATE TABLE `data_user` (
 --
 
 INSERT INTO `data_user` (`id_user`, `nama_lengkap`, `email`, `username`, `password`, `telp`, `alamat`, `tipe_akun`) VALUES
-(1, 'Andryan', 'andryan@gmail.com', 'andryan', '12345678', '085675xxxxx', 'Jln Hidup Baru gg L no 65', 'Owner'),
+(1, 'Andryan', 's31190080@student.ubm.ac.id', 'andryan', '12345678', '085675xxxx8', 'Jln Hidup Baru gg L no 65', 'Owner'),
 (2, 'Admin', 'admin@gmail.com', 'admin', '12345678', '-', '', 'Admin'),
 (3, 'User', 'user@gmail.com', 'user', '12345678', '-', '', 'User');
 
@@ -242,19 +247,19 @@ ALTER TABLE `data_user`
 -- AUTO_INCREMENT for table `data_barang_keluar`
 --
 ALTER TABLE `data_barang_keluar`
-  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `data_barang_masuk`
 --
 ALTER TABLE `data_barang_masuk`
-  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `data_retur_barang`
 --
 ALTER TABLE `data_retur_barang`
-  MODIFY `id_retur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_retur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `data_stock`
