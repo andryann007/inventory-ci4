e50b5da7d66333baf57db9886759f838f94a9720
Andryan <andryancoolz@gmail.cpm>
2023-05-21 18:29:14 +0700

    Modify Header (Just Name and No Account Type)

--- app/Views/templates/topbar.php
+++ app/Views/templates/topbar.php
@@ -28,7 +28,7 @@
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
         aria-haspopup="true" aria-expanded="false">
         <span class="mr-2 d-none d-lg-inline text-white">
-          Halo, <b><?php echo session()->get('nama_lengkap'); ?> (<?php echo session()->get('tipe_akun'); ?>)</b>
+          Halo, <b><?php echo session()->get('nama_lengkap'); ?></b>
         </span>
         <img class="img-profile rounded-circle" src="<?= base_url(); ?>/img/undraw_profile.svg" />
       </a>
